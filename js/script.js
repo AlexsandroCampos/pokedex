aa68c5b902a962c036efa099993a3a31ca9f1399
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2022-08-07 17:15:38 -0300

    Adicionando tratamento de erros

--- js/script.js
+++ js/script.js
@@ -9,22 +9,33 @@ const input = document.querySelector('.input__search');
 const fetchPokemon = async (pokemon) => {
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
 
-    const data = await APIResponse.json();
-
-    return data;
+    if(APIResponse.status === 200) {
+        const data = await APIResponse.json();
+        return data;
+    }
 }
 
 const renderPokemon = async (pokemon) => {
+    pokemonName.innerHTML = 'Loading...';
+    pokemonNumber.innerHTML = '';
+
     const data = await fetchPokemon(pokemon);
-    
-    pokemonName.innerHTML = data.name;
-    pokemonNumber.innerHTML = data.id;
-    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
-    input.value = '';
+
+    if(data) {
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        input.value = '';
+    } else {
+        pokemonName.innerHTML = 'Not found';
+        pokemonNumber.innerHTML = '';
+    }
 }
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
 
     renderPokemon(input.value);
-})
+});
+
+renderPokemon('1');
